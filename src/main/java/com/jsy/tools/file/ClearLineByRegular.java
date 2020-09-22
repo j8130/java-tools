7a4842f2c67efb54f9838d5a38476126f4dac9d7
j8130 <122969720@qq.com>
2020-09-22 22:56:58 +0800

    Initial commit

---
+++ src/main/java/com/jsy/tools/file/ClearLineByRegular.java
@@ -0,0 +1,12 @@
+package com.jsy.tools.file;
+
+/**
+ * @Author: jsy
+ * @Date: 2020/9/22 22:46
+ */
+public class ClearLineByRegular {
+    public static void main(String[] args) {
+
+        System.out.println("ssssss");
+    }
+}
